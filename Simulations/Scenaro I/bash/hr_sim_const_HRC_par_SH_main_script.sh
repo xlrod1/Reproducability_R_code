bef92a44d88808edcc85a1d0bab7e48d2c355205
xlrod1 <78033699+xlrod1@users.noreply.github.com>
2022-02-23 10:59:49 +0200

    delete the pbs and basg files

--- Simulations/Scenaro I/bash/hr_sim_const_HRC_par_SH_main_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in `seq 1 12`;
-do
-echo Submitting job $i
-qsub -v "j_id=$i" /a/home/cc/stud_math/axelrod1/simulations_on_cluster/New_only_two_scenarios/without_confounder/pbs/hr_sim_const_HRC_wrapper_parallel.pbs
-done
